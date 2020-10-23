ee31593ab3260e26c6edb2ae73266d0d645ebf1e
Nithin George <67939718+ngeorge6@users.noreply.github.com>
2020-10-22 23:36:07 -0500

    Update 11.c

--- 11.c
+++ 11.c
@@ -6,7 +6,7 @@ int main()
   printf("Enter number of elements\n");
   scanf("%d", &n);
 
-  printf("Enter %d integer(s)\n", n);
+  printf("Enter %d integer(s): \n", n);
 
   for (c = 0; c < n; c++)
     scanf("%d", &array[c]);
